e40f89b255e6ff28be60813642b7e78d8e5a7f1a
Isaque Azevedo <isaque.azevedo@sap.com>
2019-04-26 22:14:03 -0300

    Assign start

--- routes/materials.js
+++ routes/materials.js
@@ -22,8 +22,9 @@ router.get('/new', async function(req, res) {
     res.render('newmaterial', {
       title: 'Add New Materials', 
       action: "/materials/add" ,
-      projectlist: docs,
+      projectlist: projectlist,
       userlist: userlist,
+      assign: {},
       material: {
       }
     });
@@ -37,6 +38,12 @@ router.get('/:materialId/edit', async function(req, res) {
   const material = await collection.findOne({
     _id: req.params.materialId
   });
+  const assignCollection = db.get('materialprojectcollection');
+  console.log(material);
+  const assign = await assignCollection.find({
+    'material': material._id
+  });
+  console.log(assign);
   const projectCollection = db.get('projectcollection');
   const userCollection = db.get('usercollection');
   const projectlist = await projectCollection.find({});
@@ -46,7 +53,41 @@ router.get('/:materialId/edit', async function(req, res) {
     action: "/materials/update",
     projectlist: projectlist,
     userlist: userlist,
+    assign: assign,
     material
+  });
+});
+
+/* GET Assign Materials to Projects page. */
+router.get('/assignProject', async function(req, res) {
+  const db = req.db;
+  const materialCollection = db.get('materialcollection');
+  const projectCollection = db.get('projectcollection');
+  const materiallist = await materialCollection.find({});
+  const projectlist = await projectCollection.find({});
+  res.render('materialProject', {
+    title: 'Assign Project to Material', 
+    action: "/materials/relateProject" ,
+    projectlist: projectlist,
+    materiallist: materiallist,
+    assign: {
+    }
+  });
+});
+
+/* POST to relate Materials and Projects Service */
+router.post('/relateProject', function(req, res) {
+  const db = req.db;
+  const assign = req.body;
+  delete assign._id;
+  const collection = db.get('materialprojectcollection');
+  collection.insert(assign, function (err) {
+      if (err) {
+        res.send("There was a problem adding the information to the database.");
+      }
+      else {
+        res.redirect("/materials");
+      }
   });
 });
 
