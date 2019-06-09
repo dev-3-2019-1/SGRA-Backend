386a2c47ec0d269f49a2769f6a70958502b22405
Nathan Schneider <nathan.schneider@sap.com>
2019-06-09 01:39:46 -0300

    Create cronJobs file with violation alert message cron job

---
+++ cronJobs.js
@@ -0,0 +1,51 @@
+var CronJob = require('cron').CronJob;
+var moment = require('moment');
+
+module.exports = async function(db) {
+  new CronJob('0 9 * * *', async function() { // replace with * * * * * to run every minute for testing
+    const now = new Date();
+    const weekFromNow = new Date();
+    weekFromNow.setDate(now.getDate() + 7);
+    const requirementsAboutToExpire = await db.get('projectRequirementsCollection').find({
+      $and: [
+        {
+          solvedate: {
+            $gt: now,
+            $lt: weekFromNow
+          }
+        },
+        {
+          $or: [
+            {
+              status: {
+                $eq: "OPEN"
+              }
+            },
+            {
+              status: {
+                $eq: "PENDING"
+              }
+            }
+          ]
+        }
+      ]
+    });
+  
+    if (requirementsAboutToExpire && requirementsAboutToExpire.length) {
+      const messages = requirementsAboutToExpire.map((a) => {
+        return {
+          messagetext: `Requirement ${a.requirementName} of project ${a.projectName} is in status ${a.status} and will expire in ${moment.utc(a.solvedate).format("DD-MM-YYYY")}. Please expedite.`,
+          sender: "SYSTEM",
+          receivers: a.responsibleUser,
+          project: a.proj,
+        };
+      });
+      db.get("messagecollection").insert(messages, (err) => {
+        if (err) {
+          console.log("Something went wrong while running requirements CRON job:");
+          console.log(err);
+        }
+      });
+    }
+  }, null, true);
+};
